a18fc3a96616d4a2067ec9d1a64cc7c4a040013f
juliusmarminge <julius0216@outlook.com>
2024-03-28 22:45:46 +0100

    validate env on build

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+import createJiti from 'jiti'
+import { fileURLToPath } from 'node:url'
+
+createJiti(fileURLToPath(import.meta.url))('./src/env.ts')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
